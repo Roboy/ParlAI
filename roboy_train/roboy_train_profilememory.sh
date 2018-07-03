f4a28b3dc54fb2a2a07740ad26dc11fb828d5adb
Christoph <34374252+christqoh@users.noreply.github.com>
2018-07-03 19:00:56 +0200

    finetuning 180703_1200

--- roboy_train/roboy_train_profilememory.sh
+++ roboy_train/roboy_train_profilememory.sh
@@ -4,11 +4,11 @@
 # clear tmp to prevent loading old runs
 rm -rf tmp/
 
-python3 train_roboy.py --dict-include-valid False -hs 1024 -emb 300 -nl 2 -bs 64 -lr 0.01 -dr 0.4 -tr -1 -dec same -opt adam -ltim 15 -vtim 600 -vme 1000 -vp 20 -vmt f1 -vmm max -vcut 1.0 -dbf True -stim 900 &>../data/models/convai2/profilememory/logging/log.txt
+python3 train_roboy.py --model-file ~/Documents/Roboy/ss18_showmaster/ParlAI/data/models/convai2/profilememory/180703_1200/roboy_profilemem --dict-include-valid False -hs 1024 -emb 300 -nl 2 -bs 64 -lr 0.001 -dr 0.4 -tr -1 -dec same -opt adam -ltim 15 -vtim 600 -vme 1000 -vp 50 -vmt f1 -vmm max -vcut 1.0 -dbf True -stim 900 &>../data/models/convai2/profilememory/logging/log.txt
 
 #--dict-file models:convai2/profilememory/profilememory_convai2.dict
 #-tok split
-# --model-file ~/ss18_showmaster/ParlAI/data/models/profilememory_convai2_model
+#--model-file ~/ss18_showmaster/ParlAI/data/models/convai2/profilememory/180703_1200/roboy_profilemem
 ############################# Legend #############################
 ############################# Model #############################
 # Model
