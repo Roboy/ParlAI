fff42241a92e777e8fa76c2d3bd2554d3ac5ca5d
Christoph <34374252+christqoh@users.noreply.github.com>
2018-07-05 15:11:37 +0200

    new hyper

--- roboy_train/roboy_train_profilememory.sh
+++ roboy_train/roboy_train_profilememory.sh
@@ -4,7 +4,7 @@
 # clear tmp to prevent loading old runs
 rm -rf /tmp/
 
-python3 train_roboy.py -hs 2048 -emb 300 -nl 2 -bs 64 -lr 0.01 -dr 0.2 -ltim 30 -vtim 900 -vp 25 -vmt f1 -vmm max -tr -1 &>../data/models/convai2/profilememory/logging/log.txt
+python3 train_roboy.py -hs 1024 -emb 300 -nl 2 -bs 64 -lr 0.001 -dr 0.4 -ltim 30 -vtim 900 -vp 25 -vmt f1 -vmm max -tr -1 &>../data/models/convai2/profilememory/logging/log.txt
 
 # --model-file ~/ss18_showmaster/ParlAI/data/models/convai2/profilememory/180703_1200/roboy_profilemem
 # --dict-file ~/ss18_showmaster/ParlAI/data/models/convai2/profilememory/180703_1200/roboy_profilemem.dict
