f8c24941d3da5231e7b2dcda661d79455f88b3cf
Christoph <34374252+christqoh@users.noreply.github.com>
2018-08-10 16:20:22 +0200

    next

--- projects/roboy/train/train_script.sh
+++ projects/roboy/train/train_script.sh
@@ -4,7 +4,7 @@
 # clear tmp to prevent loading old runs
 rm -rf /tmp/
 
-python3 roboy_training.py -hs 768 -emb 300 -nl 2 -bs 128 -lr 0.001 -dr 0.1 -ltim 15 -stim 900 -vtim 180 -vp 10 -vmt ppl -vmm min -tr -1 &>../../../data/models/convai2/profilememory/logging/log.txt
+python3 roboy_training.py -hs 1536 -emb 300 -nl 2 -bs 64 -lr 0.001 -dr 0.3 -ltim 15 -stim 900 -vtim 180 -vp 10 -vmt ppl -vmm min -tr 100 &>../../../data/models/convai2/profilememory/logging/log.txt
 #--model-file ~/ss18_showmaster/ParlAI/data/models/convai2/profilememory/180810_1130/roboy_profilemem --dict-file ~/ss18_showmaster/ParlAI/data/models/convai2/profilememory/180810_1130/roboy_profilemem.dict
 # --dict-include-valid False
 # -dbf True
