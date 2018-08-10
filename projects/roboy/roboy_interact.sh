7f7b4efd55fadb81d6c359d67741283123e8086f
Christoph <34374252+christqoh@users.noreply.github.com>
2018-08-10 13:39:00 +0200

    updated paths

--- projects/roboy/roboy_interact.sh
+++ projects/roboy/roboy_interact.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # clear
 
-python projects/convai2/baselines/profilememory/interactive.py --model-file data/models/convai2/profilememory/180718_1000/roboy_profilemem --dict-file data/models/convai2/profilememory/180718_1000/roboy_profilemem.dict --attention general --personachat-useprevdialog
+python projects/convai2/baselines/profilememory/interactive.py --model-file data/models/convai2/profilememory/180810_1130/roboy_profilemem --dict-file data/models/convai2/profilememory/180810_1130/roboy_profilemem.dict --attention general --personachat-useprevdialog
