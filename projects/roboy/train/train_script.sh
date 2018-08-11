55cc1d083e5dad12761c2431a1cef60698657857
Christoph <34374252+christqoh@users.noreply.github.com>
2018-08-11 17:27:25 +0200

    finetuning 1500

--- projects/roboy/train/train_script.sh
+++ projects/roboy/train/train_script.sh
@@ -4,9 +4,7 @@
 # clear tmp to prevent loading old runs
 rm -rf /tmp/
 
-python3 roboy_training.py -hs 1536 -emb 300 -nl 1 -bs 128 -lr 0.001 -dr 0.3 -ltim 15 -stim 900 -vtim 180 -vp 10 -vmt ppl -vmm min -tr 100 &>../../../data/models/convai2/profilememory/logging/log.txt
-
-##--model-file ~/ss18_showmaster/ParlAI/data/models/convai2/profilememory/180810_1940/roboy_profilemem --dict-file ~/ss18_showmaster/ParlAI/data/models/convai2/profilememory/180810_1940/roboy_profilemem.dict
+python3 roboy_training.py -hs 1536 -emb 300 -nl 1 -bs 128 -lr 0.0001 -dr 0.4 -ltim 15 -stim 900 -vtim 180 -vp 10 -vmt ppl -vmm min -tr 100 &>../../../data/models/convai2/profilememory/logging/log.txt --model-file ~/ss18_showmaster/ParlAI/data/models/convai2/profilememory/180811_1500/roboy_profilemem --dict-file ~/ss18_showmaster/ParlAI/data/models/convai2/profilememory/180811_1500/roboy_profilemem.dict
 # --dict-include-valid False
 # -dbf True
 #-tok split
