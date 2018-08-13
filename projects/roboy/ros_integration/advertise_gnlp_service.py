2d323396bc8d291737b49e932f428e37b4bcd986
Christoph <34374252+christqoh@users.noreply.github.com>
2018-08-13 16:12:48 +0200

    removed unused

--- projects/roboy/ros_integration/advertise_gnlp_service.py
+++
@@ -1,8 +0,0 @@
-# /bin/bash
-export PYTHONPATH=
-export PATH=~/anaconda3/bin:$PATH
-
-## get this to point to venv for ParlAI
-source ~/Documents/Roboy/venv_ParlAI/bin/activate
-
-python projects/convai2/baselines/profilememory/interactive.py
