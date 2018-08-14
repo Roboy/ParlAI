1d23f663a942dae2349a57a0f984f510ed301fb5
Christoph <34374252+christqoh@users.noreply.github.com>
2018-08-14 16:25:57 +0200

    added to interact script

--- projects/roboy/roboy_interact.sh
+++ projects/roboy/roboy_interact.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # clear
 
-python projects/convai2/baselines/profilememory/interactive.py --model-file data/models/convai2/profilememory/180810_1940/roboy_profilemem --dict-file data/models/convai2/profilememory/180810_1940/roboy_profilemem.dict --attention general --personachat-useprevdialog
+python projects/convai2/baselines/profilememory/interactive.py --model-file data/models/convai2/profilememory/180810_1940/roboy_profilemem --dict-file data/models/convai2/profilememory/180810_1940/roboy_profilemem.dict --interactive-mode --attention general --personachat-useprevdialog
