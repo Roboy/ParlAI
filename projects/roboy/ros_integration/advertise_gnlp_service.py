0786b864f26d18c722d358f533710dd6cd75267f
Christoph <34374252+christqoh@users.noreply.github.com>
2018-08-15 13:09:40 +0200

    adapted paths

--- projects/roboy/ros_integration/advertise_gnlp_service.py
+++ projects/roboy/ros_integration/advertise_gnlp_service.py
@@ -5,4 +5,4 @@ export PATH=~/anaconda3/bin:$PATH
 ## get this to point to venv for ParlAI
 source /home/roboy/workspace/venv_ParlAI/bin/activate
 
-python gnlp_ros_srv.py
+python /home/roboy/workspace/Roboy/src/ParlAI/projects/roboy/ros_integration/gnlp_ros_srv.py
